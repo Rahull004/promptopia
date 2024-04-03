35d9bf6c6d577efa49aac26f019869864b6da603
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-04-03 16:57:28 +0530

    completing next-auth functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import { connectToDB } from "@/utils/database";
+import User from "@/models/user";
 
 const handler = NextAuth({
     providers: [GoogleProvider({
@@ -8,29 +9,37 @@ const handler = NextAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email });
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({ email: session.user.email });
 
-            const userExists = await User.findOne({ email: profile.email });
+            session.user.id = sessionUser._id.toString();
 
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+
+                const userExists = await User.findOne({ email: profile.email });
+
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
+
+
 })
 
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 function Nav() {
-  const isUserLogin = true;
+  const {data: session} = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown,setToggleDropdown] = useState(true);
 
   useEffect(() => {
-    const setProviders = async () => {
+      const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -34,7 +34,7 @@ function Nav() {
         <p className="logo_text">Promtopia</p>
       </Link>
       <div className="sm:flex hidden">
-        {isUserLogin ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -45,7 +45,7 @@ function Nav() {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -73,7 +73,7 @@ function Nav() {
       {/* Mobile Navigation */}
 
       <div className="sm:hidden flex relative">
-        {isUserLogin ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
