0bda0eae99b30e5f2dfec46736c021432f0d45bd
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-04-04 19:04:21 +0530

    completing functionality of feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
 
-function Feed() {
+import { useState, useEffect } from "react";
+import PromptCardList from "./PromptCardList";
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]); 
+  const handleSearchChange = (e) => {
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () =>{
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, []);
   return (
-    <div>Feed</div>
-  )
-}
+    <section className="feed">
+      <form className=" relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tag or a username "
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
 
-export default Feed
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
