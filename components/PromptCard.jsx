c43a76c879669750799387c05c1a26c857344dc3
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-04-06 14:54:12 +0530

    adding search prompt functionality in home page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,27 +1,38 @@
 "use client";
 
-import React from "react";
-import Image from "next/image";
 import { useState } from "react";
+import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
-  const [copied, setCopied] = useState("");
   const router = useRouter();
+
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => {
-      setCopied("");
-    }, 3000);
+    setTimeout(() => setCopied(false), 3000);
   };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer ">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -29,11 +40,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className="rounded-full object-contain"
           />
+
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
-            <p className="font-satoshi font-normal text-gray-500">
+            <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
             </p>
           </div>
@@ -41,18 +53,19 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
         <div className="copy_btn" onClick={handleCopy}>
           <Image
-            alt="copy_icon"
-            width={12}
-            height={12}
             src={
               copied === post.prompt
-                ? "/assets/icons/copied.svg"
+                ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            width={12}
+            height={12}
           />
         </div>
       </div>
-      <p className="my-4 font-satoshi text-sm text-gray-700 ">{post.prompt}</p>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
